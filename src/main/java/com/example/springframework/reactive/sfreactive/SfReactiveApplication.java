ca9dceb6ba51798175313dc73d2d6e7e915c65d2
Martin Champarini <martin.champarini.dev@gmail.com>
2018-10-28 19:00:34 -0300

    First Commit: added spring framework

---
+++ src/main/java/com/example/springframework/reactive/sfreactive/SfReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.example.springframework.reactive.sfreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfReactiveApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springframework/reactive/sfreactive/SfReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springframework.reactive.sfreactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfReactiveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
